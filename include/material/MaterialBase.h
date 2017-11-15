5fedba7d771390d3431d3edc43ec72048ca1684e
hillyuan <hill.yuanjp@gmail.com>
2017-11-15 13:52:54 +0900

    aa

--- include/material/MaterialBase.h
+++ include/material/MaterialBase.h
@@ -18,10 +18,13 @@
 #ifndef _materialbase_h_
 #define _materialbase_h_
 
+
+#include <array>
 #include "factory.hpp"
 
 namespace GNCLib
 {
+	enum StainType {Inifinite=0, GreenLagrangian, Log};
   /*
      Values of shape functions and shape derivatives in some specified points,
      which are generally defined by quadrature strategy, are stored in this table.
@@ -40,7 +43,7 @@ namespace GNCLib
           static unsigned n_status_parameters=0; // intermediate variables needed. e.g 6 plastic strain components
 	   
 	   protected:
-	      std::unordered_map<string, vector<double> > dictionary;
+	      std::unordered_map<string, vector<double> > dictionary;   // temp, when reading?
 	   
 	   public:
           virtual void ConstitutiveMatrix()=0;
@@ -48,25 +51,137 @@ namespace GNCLib
    
    // A preprocessor define used by derived classes
 	#define REGISTER_CLASS(NAME, TYPE) static Registrar registrar(NAME, [](void) -> MaterialBase * { return new TYPE();});
+	
+   class Elastic: public MaterialBase
+   {};
    
  
-   class IsoElastic: public MaterialBase 
+   class IsotropicElastic: public Elastic 
    {
+	   typedef std::array<double, 2>  constants_type;
+       typedef std::function< constants_type(std::vetcor<double>)> material_constants;
+	   // how to define the fucntion of material_constant, table, function?
 	   public:
           static unsigned n_parameters = 2;      // Youngs Modulus, Poisson's ratio
           static unsigned n_status = 0;          // elastic only
           static unsigned n_status_parameters=0; // no state changs ?
 	   
 	   public:
-		  IsoElastic() {
-			  auto instance = BaseFactory<IsoElastic>:: Instance()->Create("ELASTIC");
-			  REGISTER_CLASS("ELASTIC", IsoElastic);
+		  IsotropicElastic() {
+			  auto instance = BaseFactory<IsotropicElastic>:: Instance()->Create("ELASTIC");
+			  REGISTER_CLASS("ELASTIC", IsotropicElastic);
 		  };
           virtual void ConstitutiveMatrix();
+		  
+	   private:
+	      material_constants mc;
    };
    
-   class MisesPlastic: public MaterialBase 
+   class OrthotropicElastic: public Elastic
    {
+	   typedef std::array<double, 9>  constants_type;
+       typedef std::function< constants_type(std::vetcor<double>)> material_constants;
+	   
+	   public:
+          static unsigned n_parameters = 2;      // Youngs Modulus, Poisson's ratio
+          static unsigned n_status = 0;          // elastic only
+          static unsigned n_status_parameters=0; // no state changs ?
+	   
+	   public:
+		  OrthotropicElastic() {
+			  auto instance = BaseFactory<OrthotropicElastic>:: Instance()->Create("ELASTIC");
+			  REGISTER_CLASS("ELASTIC", OrthotropicElastic);
+		  };
+          virtual void ConstitutiveMatrix();
+		  
+       private:
+	      material_constants mc;
+   };
+   
+   class MonneyRivlin: public Elastic
+   {
+	   typedef std::array<double, 3>  constants_type;               // C10, C01, D1
+       typedef std::function< constants_type(std::vetcor<double>)> material_constants;
+	   
+	   public:
+          static unsigned n_parameters = 3;      
+          static unsigned n_status = 0;          
+          static unsigned n_status_parameters=0; 
+	   
+	   public:
+		  OrthotropicElastic() {
+			  auto instance = BaseFactory<OrthotropicElastic>:: Instance()->Create("ELASTIC");
+			  REGISTER_CLASS("ELASTIC", OrthotropicElastic);
+		  };
+          virtual void ConstitutiveMatrix();
+		  
+       private:
+	      material_constants mc;
+   };
+   
+   struct YieldFunction
+   {
+	   typedef eigen::TensorFixedSize<double, Size<3,3>> tensor_type;
+       typedef std::array<double,6> array_type;
+	   
+	   virtual array_type NormalDirection(array_type& stress)=0;      // strain for strain type function
+	   virtual tensor_type NormalDirection(tensor_type& stress)=0;
+	   
+	   virtual double FunctionVal(array_type& stress)=0;      // strain for strain type function
+	   virtual double FunctionVal(tensor_type& stress)=0;
+   };
+   
+   class Mises: public YieldFunction
+   {
+	   typedef double  constants_type;                   // initial yielding stress
+       typedef std::function< constants_type(std::vetcor<double>)> material_constants;
+	   
+	   array_type NormalDirection(array_type& stress);      // strain for strain type function
+	   tensor_type NormalDirection(tensor_type& stress);
+	   
+	   double FunctionVal(array_type& stress);      // strain for strain type function
+	   double FunctionVal(tensor_type& stress);
+	   
+	   private:
+	      material_constants mc;
+   };
+   
+   class DruckerPrager: public YieldFunction
+   {
+	   typedef std::array<double, 2>  constants_type;      // c, fai
+       typedef std::function< constants_type(std::vetcor<double>)> material_constants;
+	   
+	   array_type NormalDirection(array_type& stress);      // strain for strain type function
+	   tensor_type NormalDirection(tensor_type& stress);
+	   
+	   double FunctionVal(array_type& stress);      // strain for strain type function
+	   double FunctionVal(tensor_type& stress);
+	   
+	   private:
+	      material_constants mc;
+   };
+   
+   class MohrCoulomb: public YieldFunction
+   {
+	   typedef std::array<double, 2>  constants_type;      // c, fai
+       typedef std::function< constants_type(std::vetcor<double>)> material_constants;
+	   
+	   array_type NormalDirection(array_type& stress);      // strain for strain type function
+	   tensor_type NormalDirection(tensor_type& stress);
+	   
+	   double FunctionVal(array_type& stress);      // strain for strain type function
+	   double FunctionVal(tensor_type& stress);
+	   
+	   private:
+	      material_constants mc;
+   };
+   
+   template<typename CYield>                 // kinematic type?
+   class Plastic: public MaterialBase 
+   {
+	   typedef std::function< double(std::vetcor<double>)> yield;       //hardening rule here
+	   typedef std::function< double(std::vetcor<double>)> kinematic;   //kinematic hardening rule here
+	   
 	   public:
           static unsigned n_parameters = 2;      // Youngs Modulus, Poisson's ratio
           static unsigned n_status = 1;          // 0: elastic/ 1: plastic
@@ -74,6 +189,11 @@ namespace GNCLib
 		  
 	   public:
           virtual void ConstitutiveMatrix();
+		  
+	   private:
+		  Elastic* Elastic_pt;
+		  yield y;
+		  kinematic k;
    };
 
 
