a1f1635539cde15aa06707974467e17e124478ed
hillyuan <hillyuan.jp@gmail.com>
2018-09-17 22:52:48 +0900

    add vtk menu

--- GUI/vtkmain.py
+++ GUI/vtkmain.py
@@ -15,6 +15,8 @@ class vtkPanel(wx.Panel):
         self.SetSizer(self.sizer)
         self.Layout()
         self.isploted = False
+		
+        self.filename=""
             
     def renderthis(self):
             # open a window and create a renderer
@@ -28,7 +30,7 @@ class vtkPanel(wx.Panel):
             # o take the polygonal data from the vtkConeSource and
             # create a rendering for the renderer.
             coneMapper = vtk.vtkPolyDataMapper()
-            coneMapper.SetInputData(cone.GetOutput())
+            coneMapper.SetInputConnection(cone.GetOutputPort())
  
             # create an actor for our scene
             coneActor = vtk.vtkActor()
@@ -60,23 +62,7 @@ class TextPanel(wx.Panel):
         layout = wx.BoxSizer(wx.HORIZONTAL)
         layout.Add(calc_text, 1)
         self.SetSizer(layout)
-            
-class MainMenu(wx.MenuBar):
-
-    def __init__(self):
-
-        wx.MenuBar.__init__(self)
-        
-        menu_file = wx.Menu()
-        menu_file.Append(wx.ID_ANY, u"Save")
-        menu_file.Append(wx.ID_ANY, u"Exit")
-        menu_edit = wx.Menu()
-        menu_edit.Append(wx.ID_ANY, u"Copy")
-        menu_edit.Append(wx.ID_ANY, u"Paste")
-        
-        self.Append(menu_file, u"File")
-        self.Append(menu_edit, u"Edit")
- 
+             
 class MainFrame(wx.Frame):
     def __init__(self,parent,title):
         wx.Frame.__init__(self,parent,title=title,size=(650,600), style=wx.MINIMIZE_BOX|wx.SYSTEM_MENU|
@@ -90,10 +76,78 @@ class MainFrame(wx.Frame):
         self.statusbar = self.CreateStatusBar()
         self.statusbar.SetStatusText("Click on the Plot Button")
         
-        self.SetMenuBar(MainMenu())
+        self.makeMenuBar()
          
         self.plotbut = wx.Button(self.p2,-1,"plot", size=(40,20),pos=(10,10))
         self.plotbut.Bind(wx.EVT_BUTTON,self.plot)
+		
+    def makeMenuBar(self):
+        """
+        A menu bar is composed of menus, which are composed of menu items.
+        This method builds a set of menus and binds handlers to be called
+        when the menu item is selected.
+        """
+
+        # Make a file menu with Hello and Exit items
+        fileMenu = wx.Menu()
+        # The "\t..." syntax defines an accelerator key that also triggers
+        # the same event
+        openItem = fileMenu.Append(wx.ID_OPEN)
+        helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H",
+                "Help string shown in status bar for this menu item")
+        fileMenu.AppendSeparator()
+        # When using a stock ID we don't need to specify the menu item's
+        # label
+        exitItem = fileMenu.Append(wx.ID_EXIT)
+
+        # Now a help menu for the about item
+        helpMenu = wx.Menu()
+        aboutItem = helpMenu.Append(wx.ID_ABOUT)
+
+        # Make the menu bar and add the two menus to it. The '&' defines
+        # that the next letter is the "mnemonic" for the menu item. On the
+        # platforms that support it those letters are underlined and can be
+        # triggered from the keyboard.
+        menuBar = wx.MenuBar()
+        menuBar.Append(fileMenu, "&File")
+        menuBar.Append(helpMenu, "&Help")
+
+        # Give the menu bar to the frame
+        self.SetMenuBar(menuBar)
+
+        # Finally, associate a handler function with the EVT_MENU event for
+        # each of the menu items. That means that when that menu item is
+        # activated then the associated handler function will be called.
+        self.Bind(wx.EVT_MENU, self.OnOpen, openItem)
+        self.Bind(wx.EVT_MENU, self.OnHello, helloItem)
+        self.Bind(wx.EVT_MENU, self.OnExit,  exitItem)
+        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
+
+
+    def OnExit(self, event):
+        """Close the frame, terminating the application."""
+        self.Close(True)
+        
+    def OnOpen(self, event):
+        openFileDialog = wx.FileDialog(self, "Open Mesh file", "", self.p1.filename,
+                                       "*.stl", wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+             
+        if openFileDialog.ShowModal() == wx.ID_CANCEL:
+            return
+        self.p1.filename = openFileDialog.GetPath()
+
+
+    def OnHello(self, event):
+        """Say hello to the user."""
+        wx.MessageBox("Hello again from wxPython")
+
+
+    def OnAbout(self, event):
+        """Display an About Dialog"""
+        wx.MessageBox("This is a wxPython Hello World sample",
+                      "About Hello World 2",
+                      wx.OK|wx.ICON_INFORMATION)
+ 
          
  
     def plot(self,event):
