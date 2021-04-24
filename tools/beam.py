f4c193f1e80a2ae736d0659d53fb1944dfda8515
hillyuan <hill_yuan@hotmail.com>
2021-04-24 09:45:18 +0900

    add test data

--- tools/beam.py
+++ tools/beam.py
@@ -23,8 +23,9 @@ n_element = 0
 elements = np.array([], dtype=int)
 surface = np.array([], dtype=int)
 
+
 L= 10.0
-W = 0.4
+W = 0.5
 d = 0.1
 ddL = d
 dL = divmod( L, d )
@@ -38,8 +39,11 @@ if( dW[1]>0.0 ):
     ddW = W/nW
 
 nd = -1;
+nc = 0
 for i in range(0,nL+1):
     x = ddL*i
+    if( i==nL ):
+        nc1 = nd+1
     for j in range(0,nW+1):
         y = j*ddW
         xyz = np.append(xyz, [x, y])
@@ -49,11 +53,12 @@ for i in range(0,nL+1):
         if( i==nL ):
             ndright = np.append(ndright, nd)
 
+
 nd = 0
 ne = 0
 for i in range(0,nL):
     for j in range(0,nW):
-        elements = np.append(elements, [nd,nd+1,nd+nW+2,nd+1+nW])
+        elements = np.append(elements, [nd,nd+nW+1,nd+2+nW,nd+1])
         nd += 1
         ne += 1
     nd += 1
@@ -78,13 +83,17 @@ for i in range(0,n_element):
 fo.write("CELL_TYPES "+str(n_element) + "\n")
 for i in range(0,n_element):
     fo.write('9\n')
-fo.write("FIELD NODESET 2\n")
+fo.write("FIELD NODESET 4\n")
 fo.write("NLEFT 1 "+str(len(ndleft)) +" int\n")
 for i in range(0,len(ndleft)):
     fo.write(str(ndleft[i])+'\n')
 fo.write("NRIGHT 1 "+str(len(ndright)) +" int\n")
 for i in range(0,len(ndright)):
     fo.write(str(ndright[i])+'\n')
+fo.write("NC0 1 1 int\n")
+fo.write('0\n')
+fo.write("NC1 1 1 int\n")
+fo.write(str(nc1)+'\n')
 fo.write("FIELD EDGESET 1\n")
 ns = int(len(surface)/2)
 fo.write("S1 2 "+str(ns) +" int\n")
